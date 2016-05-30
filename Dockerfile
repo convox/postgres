1a3ff09e3aa201b7c8c0b8b4b45b36121b52c0eb
Michael Warkentin <mwarkentin@gmail.com>
2016-05-30 15:13:09 -0400

    Further updates
    * Set a few more env vars
    * Update mysql installation / configuration

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM alpine:3.3
 
-RUN apk add --update curl mysql && \
+RUN apk add --update curl mysql mysql-client && \
     rm -rf /var/cache/apk/*
 
 RUN curl -o /usr/local/bin/gosu -sSL "https://github.com/tianon/gosu/releases/download/1.2/gosu-amd64"
 RUN chmod +x /usr/local/bin/gosu
 
 ENV MYSQL_USERNAME mysql
+ENV MYSQL_ROOT_PASSWORD password
 ENV MYSQL_PASSWORD password
 ENV MYSQL_DATABASE app
 
@@ -16,7 +17,11 @@ ENV LINK_PASSWORD password
 ENV LINK_PATH /app
 
 ENV LANG en_US.utf8
+ENV MYSQL_CONF /etc/mysql
 ENV MYSQL_DATA /var/lib/mysql
+ENV MYSQL_INITFILE /init.sql
+ENV MYSQL_RUN /run/mysqld
+ENV TERM dumb
 VOLUME /var/lib/mysql
 
 COPY docker-entrypoint.sh /
@@ -24,4 +29,4 @@ COPY docker-entrypoint.sh /
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
 EXPOSE 3306
-CMD ["mysql"]
+CMD ["mysqld"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,43 +1,35 @@
 #!/bin/sh
 chown -R mysql "$MYSQL_DATA"
 
-# if [ -z "$(ls -A "$MYSQL_DATA")" ]; then
-#     gosu postgres initdb
-#     # sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$MYSQL_DATA"/postgresql.conf
-#
-#     : ${MYSQL_USER:="mysql"}
-#     : ${MYSQL_DATABASE:=$MYSQL_USER}
-#
-#     if [ "$MYSQL_PASSWORD" ]; then
-#       pass="PASSWORD '$POSTGRES_PASSWORD'"
-#       authMethod=md5
-#     else
-#       echo "==============================="
-#       echo "!!! Use \$POSTGRES_PASSWORD env var to secure your database !!!"
-#       echo "==============================="
-#       pass=
-#       authMethod=trust
-#     fi
-#     echo
-#
-#
-#     if [ "$POSTGRES_DATABASE" != 'postgres' ]; then
-#       createSql="CREATE DATABASE $POSTGRES_DATABASE;"
-#       echo $createSql | gosu postgres postgres --single -jE
-#       echo
-#     fi
-#
-#     if [ "$POSTGRES_USER" != 'postgres' ]; then
-#       op=CREATE
-#     else
-#       op=ALTER
-#     fi
-#
-#     userSql="$op USER $POSTGRES_USER WITH SUPERUSER $pass;"
-#     echo $userSql | gosu postgres postgres --single -jE
-#     echo
-#
-#     { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA"/pg_hba.conf
-# fi
-#
-# exec gosu postgres "$@"
+# TODO: Check if mysql already configured...
+mysql_install_db --user=mysql
+
+if [ ! -d "/run/mysqld" ]; then
+  mkdir -p /run/mysqld
+fi
+chown -R mysql "$MYSQL_RUN"
+
+cat << EOF > $MYSQL_INITFILE
+USE mysql
+FLUSH PRIVILEGES
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root'
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
+UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost'
+EOF
+
+if [ "$MYSQL_DATABASE" != "" ]; then
+  echo "[i] Creating database: $MYSQL_DATABASE"
+  echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci" >> $MYSQL_INITFILE
+
+  if [ "$MYSQL_USERNAME" != "" ]; then
+    echo "[i] Creating user: $MYSQL_USERNAME with password $MYSQL_PASSWORD"
+    echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USERNAME'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" >> $MYSQL_INITFILE
+  fi
+fi
+
+chown  mysql "$MYSQL_INITFILE"
+/usr/bin/mysqld --user=mysql --init-file=$MYSQL_INITFILE
+rm -f $MYSQL_INITFILE
+
+exec gosu mysql "$@"
