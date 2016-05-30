ba38c7ea078f06bacbd4d17343ba52b2c95cac1e
Michael Warkentin <mwarkentin@gmail.com>
2016-05-30 16:01:40 -0400

    Fix up init script
    * Fix `init.sql` syntax (add end of line semicolons)
    * Create users w/ passwords in one shot
    * Launch `mysqld` as `root`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,26 +10,24 @@ fi
 chown -R mysql "$MYSQL_RUN"
 
 cat << EOF > $MYSQL_INITFILE
-USE mysql
-FLUSH PRIVILEGES
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION
-UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root'
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
-UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost'
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
 EOF
 
 if [ "$MYSQL_DATABASE" != "" ]; then
   echo "[i] Creating database: $MYSQL_DATABASE"
-  echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci" >> $MYSQL_INITFILE
+  echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $MYSQL_INITFILE
 
   if [ "$MYSQL_USERNAME" != "" ]; then
     echo "[i] Creating user: $MYSQL_USERNAME with password $MYSQL_PASSWORD"
-    echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USERNAME'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" >> $MYSQL_INITFILE
+    echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USERNAME'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $MYSQL_INITFILE
   fi
 fi
 
-chown  mysql "$MYSQL_INITFILE"
-/usr/bin/mysqld --user=mysql --init-file=$MYSQL_INITFILE
+chown mysql "$MYSQL_INITFILE"
+/usr/bin/mysqld --user=root --init-file=$MYSQL_INITFILE
 rm -f $MYSQL_INITFILE
 
 exec gosu mysql "$@"
